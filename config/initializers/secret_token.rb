3cb850a532367fd62a85df4a36235b3d15a00216
Kleon Kita <kk@cockpit360.co>
2013-10-05 14:11:41 -0400

    Initial Structure Created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfdigest::Application.config.secret_token = '4f181237941c1d354641667b4db1e68eba283cf69ef4d1c042c5256bd5a2971b1c98a017c0108891bd37040a615c4eb389b8aa2540261e41a94e808d13d245d1'
